2b1421665327b43353b4b7ec51789498d80e8d5a
Juyeong Maing <mjy9088@naver.com>
2024-10-18 13:32:20 +0900

    remove not working example

--- dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-wasm-pack build wasm-worker
-(cd web-worker && npm i && npx vite build)
-wasm-pack build wasm-main
-(cd web-main && npm i && npx vite dev)
--- web-main/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import "./style.css";
-
-import { start } from "test-241017-main/test_241017_main";
-
-start();
